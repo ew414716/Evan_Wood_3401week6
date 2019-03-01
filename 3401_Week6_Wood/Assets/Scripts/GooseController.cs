93cb252d05726a6150c670e1d8473f753b4508db
Evan W Wood <ew414716@scc-mmlab-10.local>
2019-03-01 17:47:40 -0500

    3401_Week6_Wood

---
+++ 3401_Week6_Wood/Assets/Scripts/GooseController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GooseController : MonoBehaviour
+{
+    public Rigidbody2D gooseRB;
+    public float jumpSpeed = 5;
+    public GameObject gameOverCanvas;
+    public Text scoreText;
+
+    public int currentScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("REEEEEE");
+            gooseRB.velocity = new Vector2(gooseRB.velocity.x, jumpSpeed);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("uhhhh");
+        gameOverCanvas.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentScore++;
+        scoreText.text = currentScore.ToString();
+    }
+
+
+}
